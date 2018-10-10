3254c1256f3bc2ffcaf09710362b76dc9599d58e
Omid Vatandoost <omidvatan94@gmail.com>
2018-10-10 12:36:10 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(int_arr)
+  return int_arr.sort
+end
 
+def sort_array_desc(int_arr)
+  return int_arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  return arr.sort_by {|x| x.length}
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  return arr
+end
+
+def reverse_array(arr)
+  return arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.
+end
